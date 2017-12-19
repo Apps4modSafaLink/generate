68ca1c85c267fd68cb397115cfe3684e7be91e9c
apps4mod <34486950+apps4mod@users.noreply.github.com>
2017-12-19 12:44:24 +0700

    Update Apps4modSafaLinkDownload.js

--- Apps4modSafaLinkDownload.js
+++ Apps4modSafaLinkDownload.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="https://apps4modsafalink.blogspot.com/2017/12/safa-link-apps4mod
 if(x=="2"){xxx="https://apps4modsafalink.blogspot.com/2017/12/visit-link.html"}
 if(x=="3"){xxx="https://apps4modsafalink.blogspot.com/2017/12/download.html"}
 if(x=="4"){xxx="https://apps4modsafalink.blogspot.com/2017/12/link.html"}
-if(x=="5"){xxx="https://apps4modsafalink.blogspot.com/2017/12/link-download.html"}a_to_ve[i].href="https://apps4modsafalink.blogspot.co.id/2017/12/safalink-download.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="https://apps4modsafalink.blogspot.com/2017/12/link-download.html"}a_to_ve[i].href="https://apps4modsafalink.blogspot.co.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
