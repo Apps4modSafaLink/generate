9a89db87088fc86969b9a2c3bc8880fb680ec385
apps4mod <34486950+apps4mod@users.noreply.github.com>
2017-12-19 12:52:01 +0700

    Update Apps4modSafaLinkDownload.js

--- Apps4modSafaLinkDownload.js
+++ Apps4modSafaLinkDownload.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="https://apps4modsafalink.blogspot.com/2017/12/safa-link-apps4mod_12.html"}
-if(x=="2"){xxx="https://apps4modsafalink.blogspot.com/2017/12/visit-link.html"}
-if(x=="3"){xxx="https://apps4modsafalink.blogspot.com/2017/12/download.html"}
-if(x=="4"){xxx="https://apps4modsafalink.blogspot.com/2017/12/link.html"}
-if(x=="5"){xxx="https://apps4modsafalink.blogspot.com/2017/12/link-download.html"}a_to_ve[i].href="https://apps4modsafalink.blogspot.co.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2017/12/safa-link-apps4mod_12.html"}
+if(x=="2"){xxx="2017/12/visit-link.html"}
+if(x=="3"){xxx="2017/12/download.html"}
+if(x=="4"){xxx="2017/12/link.html"}
+if(x=="5"){xxx="link-download.html"}a_to_ve[i].href="https://apps4modsafalink.blogspot.co.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
