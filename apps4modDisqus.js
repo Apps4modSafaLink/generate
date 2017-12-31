93c0054409cdc51ba2ed375fc696c0323751cbe6
apps4mod <34486950+apps4mod@users.noreply.github.com>
2017-12-31 10:18:48 +0700

    Update apps4modDisqus.js

--- apps4modDisqus.js
+++ apps4modDisqus.js
@@ -1,6 +1,5 @@
 //<![CDATA[
-var disqus_shortname="http://disqus.com/by/Apps4mod 
-";
+var disqus_shortname="http://disqus.com/by/Apps4mod";
 var disqus_url = disqus_blogger_current_url;
 
 (function () {
